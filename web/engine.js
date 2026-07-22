ebafec68c8061e75814ad3d20a29cc5ab1b7ce19
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-07-22 18:00:54 -0300

    refactor the code (add engine.js)

--- web/engine.js
+++ web/engine.js
@@ -1,3 +1,124 @@
-function game_loop() {
+import * as THREE from "https://esm.sh/three@0.185.1";
+import { map_init } from "./map.js";
+import { player_animate, player_init, player_obj_init } from "./player.js";
 
+let player = null;
+let scene = null;
+let camera = null;
+let renderer = null;
+let canvas = null;
+
+
+let camera_distance = 10;
+let camera_yaw = 0;
+let camera_pitch = 0.0;
+let mouse_down = [false, false, false];
+
+function deg(degrees) {
+  return degrees * (Math.PI / 180);
+}
+
+export function engine_load() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  canvas = renderer.domElement;
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x7cc6e7);
+  renderer.setPixelRatio(1);
+  renderer.shadowMap.enabled = true;
+  document.body.appendChild(renderer.domElement);
+
+  camera.position.y = 6;
+
+  const ambient_light = new THREE.AmbientLight(0xffffff, 0.4);
+  scene.add(ambient_light);
+
+  const light = new THREE.DirectionalLight(0xffffff, 5);
+  light.position.set(20, 30, 20);
+  light.castShadow = true;
+
+  light.shadow.mapSize.set(2048, 2048);
+
+  light.shadow.camera.left = -50;
+  light.shadow.camera.right = 50;
+  light.shadow.camera.top = 50;
+  light.shadow.camera.bottom = -50;
+  light.shadow.camera.near = 1;
+  light.shadow.camera.far = 100;
+
+  scene.add(light);
+  scene.add(light.target);
+
+  document.addEventListener("mousemove", (e) => {
+    if (mouse_down[2]) {
+      const deltay = e.movementX || 0;
+      const deltax = e.movementY || 0;
+  
+      camera_pitch += deltax * 0.005;
+      camera_yaw -= deltay * 0.005;
+      camera_pitch = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, camera_pitch));
+    }
+  });
+
+  document.addEventListener("mousedown", (e) => {
+    e.preventDefault();
+    mouse_down[e.button] = true;
+  });
+
+  document.addEventListener("mouseup", (e) => {
+    e.preventDefault();
+    mouse_down[e.button] = false;
+  });
+
+  document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+  });
+
+  document.addEventListener("wheel", (e) => {
+    e.preventDefault();
+    camera_distance += e.deltaY * 0.01;
+    camera_distance = Math.max(0, Math.min(50, camera_distance));
+  }, { passive: false });
+
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
+
+  player_obj_init(scene, () => {
+    player = player_init("test");
+  });
+}
+
+export async function engine_map_load(id) {
+  scene.traverse((obj) => {
+    if (obj.isMesh) {
+      console.log(true);
+    }
+  });
+  await map_init(scene, deg, 1);
+}
+
+export function engine_loop() {
+  requestAnimationFrame(engine_loop);
+
+  if (player) {
+    player_animate(player);
+    player.model.updateMatrixWorld(true);
+  
+    const head_world_pos = new THREE.Vector3();
+    player.parts["Head"].getWorldPosition(head_world_pos);
+    head_world_pos.y += 0.35;
+
+    camera.position.x = head_world_pos.x + camera_distance * Math.cos(camera_pitch) * Math.sin(camera_yaw);
+    camera.position.y = head_world_pos.y + camera_distance * Math.sin(camera_pitch);
+    camera.position.z = head_world_pos.z + camera_distance * Math.cos(camera_pitch) * Math.cos(camera_yaw);
+
+    camera.lookAt(head_world_pos);
+  }
+
+  renderer.render(scene, camera);
 }
